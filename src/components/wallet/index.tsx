7cfabd5c922f006878bbc3428f8b443bff759bf7
MkDierz <msyibbran24@gmail.com>
2024-05-24 00:14:02 +0700

    fix: bug on wallet when no data

--- src/components/wallet/index.tsx
+++ src/components/wallet/index.tsx
@@ -39,7 +39,7 @@ export default function Wallet() {
         </div>
         <WalletFormWarper />
       </div>
-      {(isLoading || !data) && (
+      {(isLoading || data?.length == 0) && (
         <div className="flex w-full flex-1 flex-grow rounded-lg border border-dashed shadow-sm">
           <div className="flex h-full w-full  items-center justify-center">
             <div className="flex flex-col items-center gap-2 text-center">
@@ -52,7 +52,7 @@ export default function Wallet() {
           </div>
         </div>
       )}
-      {data && (
+      {data && data.length !== 0 && (
         <ScrollArea className="w-full flex-1 overflow-y-auto rounded-md border border-dashed p-1">
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
             <WalletCards data={data} />
